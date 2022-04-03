239d8cf4150fb80cfefefaa6ff8cc3e77c5549e3
TBXark <tbxark@outlook.com>
2022-04-03 12:48:44 +0800

    run server with cloudflare-worker-adapter

--- example/server/index.js
+++ example/server/index.js
@@ -1,39 +1,13 @@
-import http from 'http';
-import {argv} from 'process';
-import fetch, {Request, Response} from 'node-fetch';
-import {MemoryCache} from '@tbxark/proxy-render/lib/cache.js';
-import {createRouter} from '../core/router.js';
+import { bindGlobal, startServer, MemoryCache  } from 'cloudflare-worker-adapter';
+import {createRouter, errorHandler} from 'proxy-server-core/router.js';
 
-global.fetch = fetch;
-global.Request = Request;
-global.Response = Response;
+bindGlobal({ 
+  ProxiesCache: new MemoryCache(),
+});
 
-function getPort(argv) {
-  if (argv.indexOf('--port') !== -1) {
-    return parseInt(argv[argv.indexOf('--port') + 1], 10);
-  }
-  return null;
-}
+addEventListener('fetch', (event) => {
+  const router = createRouter(ProxiesCache);
+  event.respondWith(router.handle(event.request).catch(errorHandler));
+});
 
-function convertIncomeToNodeFecthRequest(income, host) {
-  const {url, method, headers, body} = income;
-  return new Request(`${host}${url}`, {method, headers, body});
-}
-
-async function mergeNodeFecthResponseIntoOutcome(nodeFecthResponse, outcome) {
-  const {status, headers} = nodeFecthResponse;
-  outcome.writeHead(status, headers);
-  const res = await nodeFecthResponse.text();
-  outcome.end(res);
-}
-
-const port = getPort(argv) || 8080;
-const host = `http://localhost`;
-const router = createRouter(new MemoryCache());
-http.createServer((req, res) => {
-  router.handle(convertIncomeToNodeFecthRequest(req, host))
-      .then((response) => {
-        mergeNodeFecthResponseIntoOutcome(response, res);
-      });
-}).listen(port);
-console.log(`Server running at ${host}:${port}/`);
+startServer();
